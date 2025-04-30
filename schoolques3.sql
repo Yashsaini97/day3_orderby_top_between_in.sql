295921e2096fcaad219c190be0093304b65e01e4
Yashsaini97 <166055336+Yashsaini97@users.noreply.github.com>
2025-04-30 17:06:26 +0530

    ORDER BY, TOP, BETWEEN, IN (SQL Server version

---
+++ schoolques3.sql
@@ -0,0 +1,14 @@
+select * from Students
+order by Age ASC;
+
+select *from Students
+order by Age DESC;
+
+SELECT TOP 3 * FROM Students
+ORDER BY Age DESC;
+
+select *from Students
+where Age between 18 and 22;
+
+SELECT * FROM Students
+WHERE City IN ('Delhi', 'Mumbai');
